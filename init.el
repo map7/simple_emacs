76b2f1fcdb03d2ee93d0b162525d00a45dbfc3c5
Michael Pope <map7777@gmail.com>
2011-11-15 09:26:09 +1100

    Add autosave to the original file for org-mode, helps with syncing

--- init.el
+++ init.el
@@ -66,7 +66,11 @@
 (setq backup-inhibited t) ;; disable backup
 
 ;; Org-mode options
-(add-hook 'org-mode-hook 'turn-on-visual-line-mode)
+(add-hook 'org-mode-hook
+		  'turn-on-visual-line-mode
+		  'auto-save-mode)
+(setq auto-save-visited-file-name t)
+(setq auto-save-interval 20)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
@@ -323,5 +327,6 @@
 (add-hook 'coffee-mode-hook
   '(lambda() (coffee-custom)))
 
-
+;; Don't ask to save abbrevs
 (setq save-abbrevs 'silently)
+
