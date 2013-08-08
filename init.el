9f43b3579889c8b09ebb21c2195bb93f99c0d36f
Michael Pope <map7@corsairsolutions.com.au>
2013-08-08 22:22:51 +1000

    fix coffee-script spacing

--- init.el
+++ init.el
@@ -686,6 +686,7 @@
 (defun coffee-custom ()
   "coffee-mode-hook"
   (set (make-local-variable 'tab-width) 2))
+  (set (make-local-variable 'coffee-tab-width) 2))
 
 (add-hook 'coffee-mode-hook
   '(lambda() (coffee-custom)))
