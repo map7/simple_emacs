31475c929fc8b88bb188803f7897d80485b5c964
Michael Pope <map7@corsairsolutions.com.au>
2013-08-08 22:24:22 +1000

    fix coffeescript some more

--- init.el
+++ init.el
@@ -685,7 +685,7 @@
 (require 'coffee-mode)
 (defun coffee-custom ()
   "coffee-mode-hook"
-  (set (make-local-variable 'tab-width) 2))
+  (set (make-local-variable 'tab-width) 2)
   (set (make-local-variable 'coffee-tab-width) 2))
 
 (add-hook 'coffee-mode-hook
